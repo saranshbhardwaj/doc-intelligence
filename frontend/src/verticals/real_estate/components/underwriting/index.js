78e4b8a9857279e902f275fe3e5caf2dcd975c40
Saransh Bhardwaj <sar13821@esri.com>
2026-04-27 15:23:17 -0700

    feat: export TrustPanel from underwriting index

--- frontend/src/verticals/real_estate/components/underwriting/index.js
+++ frontend/src/verticals/real_estate/components/underwriting/index.js
@@ -13,3 +13,27 @@ export {
   UnderwritingEmptyState,
   UnderwritingMetricCard,
 } from './UnderwritingUI';
+
+export { default as KeyValueList } from './KeyValueList';
+export { default as OccupancyBadge } from './OccupancyBadge';
+export { default as SourceSupportActions } from './SourceSupportActions';
+export { default as ScenarioPanel } from './ScenarioPanel';
+export { default as ModelBasisPanel } from './ModelBasisPanel';
+export { default as TrustPanel } from './TrustPanel';
+export { default as ReturnsSection } from './ReturnsSection';
+export { default as OperationsSection } from './OperationsSection';
+export { default as EvidenceSection } from './EvidenceSection';
+export { default as MarketSection } from './MarketSection';
+export { default as DiscrepanciesSection } from './DiscrepanciesSection';
+export { default as WorkflowRail } from './wizard/WorkflowRail';
+export { default as WizardInputStage } from './wizard/WizardInputStage';
+export {
+  DOC_SLOTS,
+  INITIAL_PROJECT_DATA,
+  TAB_CONFIG,
+  VISIBLE_CITATION_FIELD_KEYS,
+  countVisibleCitations,
+  createDefaultInputs,
+  computeTabProgress,
+} from './wizard/wizardConfig';
+export * from './formatters';
